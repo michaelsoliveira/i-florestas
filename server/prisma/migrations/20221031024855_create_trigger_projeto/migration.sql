1bdfb2618f59614167480172d24162c1f6fb9301
Michael Oliveira <michaelsoliveira@gmail.com>
2022-11-08 17:10:09 -0300

    Alteração ER projeto_users

--- server/prisma/migrations/20221031024855_create_trigger_projeto/migration.sql
+++ server/prisma/migrations/20221031024855_create_trigger_projeto/migration.sql
@@ -3,14 +3,14 @@ CREATE OR REPLACE FUNCTION before_insert_update_projeto()
 $$
 DECLARE id_exists TEXT;
 BEGIN
-	SELECT id INTO id_exists FROM projeto;
+SELECT id_projeto INTO id_exists FROM projeto_users WHERE id_user = NEW.id_user;
     IF NOT FOUND THEN
-		NEW.active := TRUE;
-	END IF;
-	
-	IF NEW.active = TRUE THEN
-		UPDATE projeto set active = FALSE WHERE id != NEW.id AND id_empresa = NEW.id_empresa;
-	
+NEW.active := TRUE;
+END IF;
+
+IF NEW.active = TRUE THEN
+UPDATE projeto_users set active = FALSE WHERE id_projeto != NEW.id_projeto AND id_user = NEW.id_user;
+
 END IF;
 RETURN NEW;
 
@@ -20,6 +20,6 @@ $$
 LANGUAGE 'plpgsql';
 
 CREATE TRIGGER update_projeto
-	BEFORE INSERT OR UPDATE ON projeto
-	FOR EACH ROW
-	EXECUTE FUNCTION before_insert_update_projeto();
+BEFORE INSERT OR UPDATE ON projeto
+FOR EACH ROW
+EXECUTE FUNCTION before_insert_update_projeto();
