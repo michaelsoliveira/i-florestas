02ec2496e66ab8d1b6d68faeb0b070acca482b6b
Michael Oliveira <michaelsoliveira@gmail.com>
2022-12-07 20:13:07 -0300

    Correcoes em Especies

--- client/components/especie/Especies.tsx
+++ client/components/especie/Especies.tsx
@@ -25,19 +25,16 @@ const Especies = ({ currentEspecies, onPageChanged, orderBy, order, changeItemsP
     const { visible } = store
     const { setLoading } = useContext(LoadingContext)
 
-    const ObjectsToCsv = require('objects-to-csv')
-
     const styleDelBtn = 'bg-red-600 hover:bg-red-700 focus:ring-red-500'
     const especieById = useCallback((id?: string) => {
         return currentEspecies.find((especie: EspecieType) => especie.id === id)
     }, [currentEspecies])
 
     const deleteEspecie = useCallback(async (id?: string) => {
-        
         try {
             client.delete(`/especie/single/${id}`)
                 .then((response: any) => {
-                    const { data: error, message } = response
+                    const { error, message } = response.data
                     if (!error) {
                         alertService.success(message)
                         loadEspecies()
@@ -186,17 +183,17 @@ const Especies = ({ currentEspecies, onPageChanged, orderBy, order, changeItemsP
                 <div className="flex flex-row">
                     <a
                         onClick={openFile}
-                        className="bg-indigo hover:bg-indigo-dark text-green-700 font-bold py-2 px-4 w-full inline-flex items-center lg:w-96"
+                        className="bg-indigo hover:bg-indigo-dark text-green-700 font-bold py-2 px-4 w-full inline-flex items-center hover:cursor-pointer"
                     >
                         <svg className="fill-green-700 w-6 h-6" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <path d="M0 0h24v24H0z" fill="none"/>
                             <path d="M9 16h6v-6h4l-7-7-7 7h4zm-4 2h14v2H5z"/>
                         </svg>
-                        <span className="ml-2">{uploading ? "Importando..." : "Importar Espécies"}</span>
+                        <span className="ml-2">{uploading ? "Importando..." : "Importar"}</span>
                     </a>
                     <a
                         onClick={handleImportTemplate}
-                        className="bg-indigo hover:bg-indigo-dark text-green-700 font-bold py-2 px-4 w-full inline-flex items-center"
+                        className="bg-indigo hover:bg-indigo-dark text-green-700 font-bold py-2 px-4 w-full inline-flex items-center hover:cursor-pointer"
                     >
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m.75 12l3 3m0 0l3-3m-3 3v-6m-1.5-9H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
