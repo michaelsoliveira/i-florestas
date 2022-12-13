1d8dbb9e661780a8e53285f11f147418951d96b2
Michael Oliveira <michaelsoliveira@gmail.com>
2022-12-13 12:24:17 -0300

    Criando a Entidade de Arvore

---
+++ server/src/controllers/ArvoreController.ts
@@ -0,0 +1,101 @@
+import { Request, Response } from "express";
+import categoriaService from "../services/CategoriaService";
+
+export class ArvoreController {
+    async store(request : Request, response: Response) : Promise<Response> {
+        try {    
+            const categoria = await categoriaService.create(request.body)
+            return response.json({
+                error: false,
+                categoria,
+                message: `Árvore cadastrada com SUCESSO!!!`
+            })
+
+        } catch (error) {
+            return response.json({
+                error: true,
+                especie: null,
+                message: error.message
+            })
+        }
+    }
+
+     async update(request : Request, response: Response) : Promise<Response> {
+        const { id } = request.params
+         try {    
+            const categoria = await categoriaService.update(id, request.body)
+            return response.json({
+                error: false,
+                categoria,
+                message: `Árvore atualizada com SUCESSO!!!`
+            })
+
+        } catch (error) {
+            return response.json({
+                error: true,
+                categoria: null,
+                message: error.message
+            })
+        }
+     }
+    
+    async delete(request: Request, response: Response): Promise<any> {
+        const { id } = request.params
+
+        try {
+            await categoriaService.delete(id)
+
+            return response.status(200).json({
+                error: false,
+                message: 'Árvore deletada com Sucesso!!!'
+            })
+        } catch (error) {
+            return response.json({
+                error: true,
+                especie: null,
+                message: error.message
+            })
+        }
+    }
+
+    async findAll(request: Request, response: Response) {
+        try {
+            const { data, perPage, page, skip, count } = await categoriaService.getAll(request.user?.id, request.query)
+
+            return response.json({
+                error: false,
+                categorias: data,
+                perPage,
+                page,
+                skip,
+                count,
+                message: null
+            })
+        } catch(error) {
+            return response.json({
+                error: false,
+                categorias: [],
+                message: null
+            })
+        }
+    }
+
+    async search(request: Request, response: Response) {
+        const { nome } = request.query
+        
+        const categorias = nome ? await categoriaService.search(nome) : await categoriaService.getAll(request.user?.id, request.query)
+
+        return response.json(categorias)
+    }
+
+    async findOne(request: Request, response: Response) {
+        const { id } = request.params
+        try {
+            const categoria = await categoriaService.findById(id)
+
+            return response.json(categoria)
+        } catch(error) {
+            return response.json(error.message)
+        }
+    }
+}
