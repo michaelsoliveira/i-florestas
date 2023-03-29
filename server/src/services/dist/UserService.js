8c5284541bbf10675e23c4b3fa3a92b51dbe9668
Administrador <user0@MacBook-Pro-de-Mac.local>
2023-03-29 13:30:15 -0300

    Normalização do POA

--- server/src/services/dist/UserService.js
+++ server/src/services/dist/UserService.js
@@ -422,17 +422,13 @@ var UserService = /** @class */ (function () {
                 email = data.email, name = data.name, message = data.message;
                 transporter = nodemailer_1["default"].createTransport({
                     service: 'gmail',
-                    host: 'localhost',
-                    secure: true,
+                    host: process.env.SMTP_HOST || 'smtp.gmail.com',
+                    secure: false,
                     port: 587,
                     auth: {
-                        type: 'OAuth2',
+                        // type: 'OAuth2',
                         user: process.env.GMAIL_USER,
-                        // pass: process.env.GMAIL_PWD,
-                        clientId: process.env.GOOGLE_CLIENT_ID,
-                        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-                        refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
-                        accessToken: accessToken
+                        pass: process.env.GMAIL_PWD
                     }
                 });
                 transporter.verify(function (error, success) {
