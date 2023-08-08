243fcf773fdca8dbd8b00212ab19f78fc3ed1ab1
Michael S Oliveira <michaelsoliveira@gmail.com>
2023-08-08 10:57:38 -0300

    Implementação do Form Obserções das Arvores

--- server/src/services/EspecieService.ts
+++ server/src/services/EspecieService.ts
@@ -287,7 +287,7 @@ class EspecieService {
             }
         } else {
             orderByTerm = {
-                [orderByElement]: order
+                [orderBy]: order
             }
         }
 
