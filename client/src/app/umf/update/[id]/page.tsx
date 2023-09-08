6e313a14116ba9d16bcc91534da999dfc5c8e9d9
Michael Oliveira <michaelsoliveira@gmail.com>
2023-09-08 12:49:53 -0300

    Next 13

--- client/src/app/umf/update/[id]/page.tsx
+++ client/src/app/umf/update/[id]/page.tsx
@@ -3,33 +3,38 @@ import withAuthentication from "@/components/utils/withAuthentication";
 import { authOptions } from "@/lib/authOptions";
 import { getServerSession } from "next-auth";
 import { ResolvingMetadata, Metadata } from 'next'
+import { redirect } from 'next/navigation'
 // import Form from "@/components/umf/Form"
 
 
-  // type Props = {
-  //   params: { id: string }
-  // }
-
-  // const getSession: any = async () => {
-  //   return await getServerSession(authOptions)
-  // }
+  type Props = {
+    params: { id: string }
+  }
    
-  // export async function generateMetadata(
-  //   { params }: Props,
-  //   parent: ResolvingMetadata
-  // ): Promise<Metadata> {
-  //   // read route params
-  //   const id = params.id
-  //   const session = await getSession()
+  export async function generateMetadata(
+    { params }: Props,
+    parent: ResolvingMetadata
+  ): Promise<Metadata | void> {
+    try {
+      // read route params
+      const id = params.id
+      const session = await getServerSession(authOptions)
+      if (session) {
+        const umf = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/umf/${id}`, { headers: { 'Authorization': `Bearer ${session?.accessToken}` } }).then((res) => res.json())
     
-  //   const umf = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/umf/${id}`, { headers: { 'Authorization': `Bearer ${session?.accessToken}` } }).then((res) => res.json())
-   
-  //   return {
-  //     title: umf.nome
-  //   }
-  // }
+        return {
+          title: umf.nome,
+          description: 'Informações da UMF'
+        }
+      } else {
+        return redirect('/login')
+      }
+    } catch (error) {
+      console.log(error)
+    }        
+  }
 
-// const pageUmf = ({ params }: { params: { id: string } }) => {
+// const pageUmf = () => {
 //     try {
       
 //         return (
