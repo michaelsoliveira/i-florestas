45ae62baeea0ec0996cfa13cc4b9febeffdb58b3
Administrador <user0@MacBook-Pro-de-Mac.local>
2023-09-08 09:42:33 -0300

    Next 13

--- client/src/app/umf/update/[id]/page.tsx
+++ client/src/app/umf/update/[id]/page.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import AddEdit from "@/components/umf/AddEdit";
 import withAuthentication from "@/components/utils/withAuthentication";
 import { authOptions } from "@/lib/authOptions";
