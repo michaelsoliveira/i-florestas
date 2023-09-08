385968f6462c75d2ae7035cf7ae3f7d673f2bb0b
Michael Oliveira <michaelsoliveira@gmail.com>
2023-09-07 23:35:44 -0300

    Next 13

--- client/src/components/umf/AddEdit.tsx
+++ client/src/components/umf/AddEdit.tsx
@@ -1,32 +1,12 @@
-import { LinkBack } from '@/components/utils/LinkBack'
-import Form from './Form'
+import { ReactNode } from 'react'
 
-const AddEdit = ({ umf }: { umf? : any}) => {
+const AddEdit = ({ children }: { children? : ReactNode}) => {
 
     return (
         <>
              <div>
                 <div className="py-6 flex flex-col justify-center sm:py-12 bg-gray-50">
-                    
-                    <div className="relative py-3 w-11/12 max-w-none lg:max-w-2xl mx-auto">
-                        <div className='flex flex-row items-center justify-between border border-custom-green text-white shadow-lg bg-custom-green py-4 sm:rounded-t-xl'>
-                            
-                            <div>
-                                <LinkBack href="/umf" className="flex flex-col relative left-0 ml-4" />
-                            </div>
-                            <div>
-                                {!umf ? (
-                                    <h1 className='text-xl'>Cadastrar UMF</h1>
-                                ): (
-                                    <h1 className='text-xl'>Editar UMF</h1>
-                                )}
-                            </div>
-                            <div></div>
-                        </div>
-                        <div className="relative p-8 bg-white shadow-sm sm:rounded-b-xl border-x border-b border-custom-green">
-                            <Form umf={umf} />
-                        </div>
-                    </div>
+                    { children }
                 </div>
             </div>
         </>
--- client/src/components/umf/Form.tsx
+++ client/src/components/umf/Form.tsx
@@ -12,6 +12,7 @@ import { Link } from '@/components/utils/Link'
 import { useAppDispatch } from '@/redux/hooks'
 import { setUmf } from '@/redux/features/umfSlice'
 import SelectEstado from '@/components/utils/SelectEstado'
+import { LinkBack } from '../utils/LinkBack'
 
 const Form = ({ umf }: { umf? : any}) => {
 
@@ -98,7 +99,22 @@ const Form = ({ umf }: { umf? : any}) => {
     }
 
     return (
-        <>
+        <div className="relative py-3 w-11/12 max-w-none lg:max-w-2xl mx-auto">
+            <div className='flex flex-row items-center justify-between border border-custom-green text-white shadow-lg bg-custom-green py-4 sm:rounded-t-xl'>
+                
+                <div>
+                    <LinkBack href="/umf" className="flex flex-col relative left-0 ml-4" />
+                </div>
+                <div>
+                    {!umf ? (
+                        <h1 className='text-xl'>Cadastrar UMF</h1>
+                    ): (
+                        <h1 className='text-xl'>Editar UMF</h1>
+                    )}
+                </div>
+                <div></div>
+            </div>
+            <div className="relative p-8 bg-white shadow-sm sm:rounded-b-xl border-x border-b border-custom-green">
             <form onSubmit={handleSubmit(onSubmit)}>
                 <div className='w-full'>
                     <FormInput
@@ -134,27 +150,28 @@ const Form = ({ umf }: { umf? : any}) => {
                     
                 </div>  
                 <div className='flex flex-col md:flex-row space-x-0 md:space-x-4'>
-                <div className='w-8/12'>
-                        <FormInput
-                            id="municipio"
-                            name="municipio"
-                            label="Município"
-                            type="text"
-                            register={register}
-                            errors={errors}
-                            className="pb-4"
-                        />
+                    <div className='w-8/12'>
+                            <FormInput
+                                id="municipio"
+                                name="municipio"
+                                label="Município"
+                                type="text"
+                                register={register}
+                                errors={errors}
+                                className="pb-4"
+                            />
+                        </div>
+                        <div className='w-4/12 pt-1'>
+                            <SelectEstado value={estado?.value && estado} callback={selectedEstado} />
+                        </div>
                     </div>
-                    <div className='w-4/12 pt-1'>
-                        <SelectEstado value={estado?.value && estado} callback={selectedEstado} />
+                    <div className='flex items-center justify-between pt-4'>
+                        <Link href="/umf" className="text-center w-2/5 bg-gray-light text-gray-700 p-3 rounded-md">Voltar</Link>
+                        <button className="w-2/5 bg-custom-green text-white p-3 rounded-md">Salvar</button>
                     </div>
-                </div>
-                <div className='flex items-center justify-between pt-4'>
-                    <Link href="/umf" className="text-center w-2/5 bg-gray-light text-gray-700 p-3 rounded-md">Voltar</Link>
-                    <button className="w-2/5 bg-custom-green text-white p-3 rounded-md">Salvar</button>
-                </div>
-            </form>
-        </>
+                </form>
+            </div>
+        </div>
     )
     
 }
